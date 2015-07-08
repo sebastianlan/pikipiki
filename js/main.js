4cae5434241cecdc022d470e9fec7f3444edbecc
sebastianlan <sebastianlan.original@gmail.com>
2015-07-08 19:26:52 +0800

    update menu sideslide javascript

--- js/main.js
+++ js/main.js
@@ -1,13 +1,3 @@
-/**
- * main.js
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2014, Codrops
- * http://www.codrops.com
- */
 (function() {
 
 	var bodyEl = document.body,
@@ -26,7 +16,6 @@
 			closebtn.addEventListener( 'click', toggleMenu );
 		}
 
-		// close the menu element if the target it´s not the menu element or one of its descendants..
 		content.addEventListener( 'click', function(ev) {
 			var target = ev.target;
 			if( isOpen && target !== openbtn ) {
